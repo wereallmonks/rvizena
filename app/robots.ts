00b7175460083c105ace98dc0a9ed1e1c7f6b663
Claudia <44302167+wereallmonks@users.noreply.github.com>
2023-12-27 18:04:15 -0800

    Initial commit

---
+++ app/robots.ts
@@ -0,0 +1,11 @@
+export default function robots() {
+  return {
+    rules: [
+      {
+        userAgent: "*",
+      },
+    ],
+    sitemap: "https://vizena.com/sitemap.xml",
+    host: "https://vizena.com",
+  };
+}
